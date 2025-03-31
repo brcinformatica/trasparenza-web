ebb57af2424510dce4f83f99fe49015185e535b2
GB Factory <37420179+gbfactory@users.noreply.github.com>
2025-03-31 10:01:16 +0200

    aggiornamento

--- src/pages/Sezione.jsx
+++ src/pages/Sezione.jsx
@@ -1,45 +1,61 @@
-import { Col, Container, Row, Icon, Accordion, AccordionItem, AccordionHeader, AccordionBody, Breadcrumb, BreadcrumbItem } from "design-react-kit";
+import {
+    Col,
+    Container,
+    Row,
+    Icon,
+    Accordion,
+    AccordionItem,
+    AccordionHeader,
+    AccordionBody,
+    Breadcrumb,
+    BreadcrumbItem
+} from "design-react-kit";
 import Sezioni from "../components/Sezioni";
-import { Link, useLocation, useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import DOMPurify from "dompurify";
 
 const Sezione = () => {
-    // const apiUrl = process.env.REACT_APP_API_URL;
-    const apiUrl = 'https://api.brcnet.it';
+    const apiUrl = "https://api.brcnet.it";
     const { codCli, id } = useParams();
 
-    const [collapseElementOpen, setCollapseElement] = useState('');
-
-    // header
+    const [collapseElementOpen, setCollapseElement] = useState("");
     const [sezione, setSezione] = useState(null);
     const [sottosezioni, setSottosezioni] = useState(null);
     const [contenuti, setContenuti] = useState(null);
 
     useEffect(() => {
-        axios.get(apiUrl + '/api/rest/v1/amt/sezione/' + codCli + '/' + id).then(res => {
-            if (res.data.success) setSezione(res.data.payload);
-        });
+        const fetchData = async () => {
+            try {
+                const [resSezione, resSottosezioni, resContenuti] = await Promise.all([
+                    axios.get(`${apiUrl}/api/rest/v1/amt/sezione/${codCli}/${id}`),
+                    axios.get(`${apiUrl}/api/rest/v1/amt/sottosezioni/${codCli}/${id}`),
+                    axios.get(`${apiUrl}/api/rest/v1/amt/contenuti/${codCli}/${id}`)
+                ]);
 
-        axios.get(apiUrl + '/api/rest/v1/amt/sottosezioni/' + codCli + '/' + id).then(res => {
-            if (res.data.success) setSottosezioni(res.data.payload);
-        });
+                if (resSezione.data.success) setSezione(resSezione.data.payload);
+                if (resSottosezioni.data.success)
+                    setSottosezioni(resSottosezioni.data.payload);
+                if (resContenuti.data.success)
+                    setContenuti(resContenuti.data.payload);
+            } catch (error) {
+                console.error("Errore nel caricamento dati:", error);
+            }
+        };
 
-        axios.get(apiUrl + '/api/rest/v1/amt/contenuti/' + codCli + '/' + id).then(res => {
-            if (res.data.success) setContenuti(res.data.payload);
-        });
-    }, [id]);
+        fetchData();
+    }, [codCli, id]);
 
-    if (!sezione || !sottosezioni || !contenuti) return (
-        <p>Caricamento in corso...</p>
-    );
+    if (!sezione || !sottosezioni || !contenuti)
+        return <p>Caricamento in corso...</p>;
 
     return (
         <Container className="my-4">
             <Breadcrumb>
                 <BreadcrumbItem>
-                    <Link to={'/' + codCli}>Amministrazione Trasparente</Link>
-                    <span className='separator'>/</span>
+                    <Link to={`/${codCli}`}>Amministrazione Trasparente</Link>
+                    <span className="separator">/</span>
                 </BreadcrumbItem>
                 <BreadcrumbItem active>{sezione.etichetta}</BreadcrumbItem>
             </Breadcrumb>
@@ -53,82 +69,107 @@ const Sezione = () => {
                         <h2 className="fs-3 mt-3">Sottosezioni</h2>
                         <div className="it-list-wrapper">
                             <ul className="it-list border-top">
-                                {sottosezioni.map((sottosezione, index) => {
-                                    return (
-                                        <li key={index}>
-                                            <Link to={'/' + codCli + '/sezione/' + sottosezione.id} className="list-item">
-                                                <div className="it-rounded-icon">
-                                                    <Icon color="primary" icon="it-folder" title="Sezione" />
-                                                </div>
+                                {sottosezioni.map((sottosezione) => (
+                                    <li key={sottosezione.id}>
+                                        <Link
+                                            to={`/${codCli}/sezione/${sottosezione.id}`}
+                                            className="list-item"
+                                        >
+                                            <div className="it-rounded-icon">
+                                                <Icon
+                                                    color="primary"
+                                                    icon="it-folder"
+                                                    title="Sezione"
+                                                />
+                                            </div>
 
-                                                <div className="it-right-zone">
-                                                    <span className="text">{sottosezione.etichetta}</span>
-                                                </div>
-                                            </Link>
-                                        </li>
-                                    )
-                                })}
+                                            <div className="it-right-zone">
+                                                <span className="text">{sottosezione.etichetta}</span>
+                                            </div>
+                                        </Link>
+                                    </li>
+                                ))}
                             </ul>
                         </div>
 
                         <h2 className="fs-3 mt-4">Contenuti</h2>
                         <Accordion>
-                            {contenuti.map((contenuto, index) => {
+                            {contenuti.map((contenuto) => {
+                                const cleanHtml = DOMPurify.sanitize(contenuto.contenuto);
+
                                 return (
-                                    <AccordionItem key={index}>
+                                    <AccordionItem key={contenuto.id}>
                                         <AccordionHeader
                                             active={collapseElementOpen === contenuto.id}
-                                            onToggle={() => setCollapseElement(collapseElementOpen !== contenuto.id ? contenuto.id : '')}
+                                            onToggle={() =>
+                                                setCollapseElement(
+                                                    collapseElementOpen !== contenuto.id
+                                                        ? contenuto.id
+                                                        : ""
+                                                )
+                                            }
                                         >
-                                            {/* <Icon className="me-2" color="primary" icon="it-file" title="Contenuto" /> */}
                                             {contenuto.titolo}
                                         </AccordionHeader>
-                                        <AccordionBody active={collapseElementOpen === contenuto.id} listClassName={'custom-class'}>
-                                            <p>{contenuto.contenuto}</p>
+                                        <AccordionBody
+                                            active={collapseElementOpen === contenuto.id}
+                                            listClassName="custom-class"
+                                        >
+                                            <div dangerouslySetInnerHTML={{ __html: cleanHtml }} />
 
                                             <ul>
-                                                <li>Pubblicato il <b>{contenuto.data_pubblicazione}</b></li>
-                                                <li>Modificato il <b>{contenuto.data_modifica || contenuto.data_pubblicazione}</b></li>
-                                                <li>Archiviazione il <b>{contenuto.data_archiviazione}</b></li>
+                                                <li>
+                                                    Pubblicato il <b>{contenuto.data_pubblicazione}</b>
+                                                </li>
+                                                <li>
+                                                    Modificato il{" "}
+                                                    <b>
+                                                        {contenuto.data_modifica ||
+                                                            contenuto.data_pubblicazione}
+                                                    </b>
+                                                </li>
+                                                <li>
+                                                    Archiviazione il{" "}
+                                                    <b>{contenuto.data_archiviazione}</b>
+                                                </li>
                                             </ul>
 
-                                            {(contenuto.allegati).length > 0 ? (
+                                            {contenuto.allegati.length > 0 ? (
                                                 <div className="mt-3">
                                                     <b>Allegati:</b>
                                                     <div className="it-list-wrapper">
                                                         <ul className="it-list">
-                                                            {contenuto.allegati.map((allegato, index) => {
-                                                                return (
-                                                                    <li key={index}>
-                                                                        <div className="list-item">
-                                                                            <div className="it-rounded-icon">
-                                                                                <Icon color="primary" icon="it-file" title="Allegato" />
-                                                                            </div>
-
-                                                                            <div class="it-right-zone">
-                                                                                <a href={"https://cdn.myscuola.it/file/trasparenza/" + codCli + "/" + allegato.path} target="_blank" class="" data-focus-mouse="false">
-                                                                                    <span class="text">{allegato.nome}</span>
-                                                                                </a>
-
-                                                                                {/* <span class="it-multiple">
-                                                                                    <a href="javascript:showlistversion(1);" aria-label="Versioni del file" class="" data-focus-mouse="false">
-                                                                                        <svg class="icon" data-bs-toggle="tooltip" title="Versioni">
-                                                                                            <use href="/assets/bootstrap-italia/svg/sprites.svg#it-restore"></use>
-                                                                                        </svg>
-
-                                                                                        1                                                                                </a>
-
-                                                                                    <a href="javascript:showpermalink('3d9312e7-3665-47c6-ad23-cdcea4c1cdf6', 2);" aria-label="Permalink">
-                                                                                        <svg class="icon" data-bs-toggle="tooltip" title="Permalink">
-                                                                                            <use href="/assets/bootstrap-italia/svg/sprites.svg#it-link"></use>
-                                                                                        </svg>
-                                                                                    </a>
-                                                                                </span> */}
-                                                                            </div>
+                                                            {contenuto.allegati.map((allegato, index) => (
+                                                                <li key={index}>
+                                                                    <div className="list-item">
+                                                                        <div className="it-rounded-icon">
+                                                                            <Icon color="primary" icon="it-file" title="Allegato" />
                                                                         </div>
-                                                                    </li>
-                                                                )
-                                                            })}
+
+                                                                        <div class="it-right-zone">
+                                                                            <a href={"https://cdn.myscuola.it/file/trasparenza/" + codCli + "/" + allegato.path} target="_blank" class="" data-focus-mouse="false">
+                                                                                <span class="text">{allegato.nome}</span>
+                                                                            </a>
+
+                                                                            {/* <span class="it-multiple">
+                                                                                <a href="javascript:showlistversion(1);" aria-label="Versioni del file" class="" data-focus-mouse="false">
+                                                                                    <svg class="icon" data-bs-toggle="tooltip" title="Versioni">
+                                                                                        <use href="/assets/bootstrap-italia/svg/sprites.svg#it-restore"></use>
+                                                                                    </svg>
+
+                                                                                    1                                                                                </a>
+
+                                                                                <a href="javascript:showpermalink('3d9312e7-3665-47c6-ad23-cdcea4c1cdf6', 2);" aria-label="Permalink">
+                                                                                    <svg class="icon" data-bs-toggle="tooltip" title="Permalink">
+                                                                                        <use href="/assets/bootstrap-italia/svg/sprites.svg#it-link"></use>
+                                                                                    </svg>
+                                                                                </a>
+                                                                            </span> */}
+                                                                        </div>
+                                                                    </div>
+                                                                </li>
+
+                                                            ))}
                                                         </ul>
                                                     </div>
                                                 </div>
@@ -137,7 +178,7 @@ const Sezione = () => {
                                             )}
                                         </AccordionBody>
                                     </AccordionItem>
-                                )
+                                );
                             })}
                         </Accordion>
                     </Container>
@@ -147,7 +188,7 @@ const Sezione = () => {
                 </Col>
             </Row>
         </Container>
-    )
-}
+    );
+};
 
 export default Sezione;
